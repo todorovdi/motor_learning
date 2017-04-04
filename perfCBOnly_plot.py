49962b6f3cbd3b9ed7214fa43014f87bebebf206
= <=>
2017-04-04 12:29:58 -0400

    fixed several errors related to python version; made changes to reach plots (no labels, ymin, ymax) and set ymin, ymax for error plots (cb)

--- perfCBOnly_plot.py
+++ perfCBOnly_plot.py
@@ -82,6 +82,8 @@ def genMainPlot(fig,ax,fnames,nums):
     ymin = 0.
     ymax = pp.y_axis_max 
     ymin = pp.y_axis_min
+    ymin = -0.3
+    ymax = 0.3
     step = (ymax-ymin) / 10.
     if "y_axis_step" in pp.paramsEnv:
         step = (float(pp.paramsEnv["y_axis_step"]) ) 
@@ -218,14 +220,15 @@ def genFigurePert(fnames,outname):
         rangePost1 = range(pp.phaseBegins[-2],pp.phaseBegins[-1])
         #genReachPlot(fig,axs[1,ind],xs[rangeAdapt1],ys[rangeAdapt1],nums[rangeAdapt1],"Adapt1",tgt=zip(x_target,y_target))
         ax=axs[1,1]
-        genReachPlot(fig,ax,xs[rangeAdapt1],ys[rangeAdapt1],nums[rangeAdapt1],figName,cbtgt=zip(x_cbtgt[rangeAdapt1],y_cbtgt[rangeAdapt1]))
+        cbtgt = list(zip(x_cbtgt[rangeAdapt1],y_cbtgt[rangeAdapt1]))
+        genReachPlot(fig,ax,xs[rangeAdapt1],ys[rangeAdapt1],nums[rangeAdapt1],figName,cbtgt=cbtgt)
 
         pdf.savefig()
         plt.close()
 
         #ax = plt.gca
         fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(30, 20))
-        genMainPlot(ax,fnames,nums)
+        genMainPlot(fig,ax,fnames,nums)
         ax.set_title(pp.paramsEnv["pdfSuffix"])
 
         pdf.savefig()
@@ -316,7 +319,7 @@ def genFigurePertMulti(dat_basenames):
 
             ax = axs[1,ind]
 
-            genReachPlot(fig,ax,xs[rangeAdapt1],ys[rangeAdapt1],nums[rangeAdapt1],figName,cbtgt=zip(x_cbtgt[rangeAdapt1],y_cbtgt[rangeAdapt1]))
+            genReachPlot(fig,ax,xs[rangeAdapt1],ys[rangeAdapt1],nums[rangeAdapt1],figName,cbtgt=list(zip(x_cbtgt[rangeAdapt1],y_cbtgt[rangeAdapt1])))
 
     if pp.multiSameGraph == 0:
         ax = axs[1,ind]
@@ -386,7 +389,7 @@ def genReachingByPhase(fname):
 
     if(len(rangePre1)>0):
         genReachPlot(fig,axs[0,0],xs[rangePre1],ys[rangePre1],nums[rangePre1],"Pre1")
-    genReachPlot(fig,axs[0,1],xs[rangeAdapt1],ys[rangeAdapt1],nums[rangeAdapt1],figName,tgt=zip(x_target[rangeAdapt1],y_target[rangeAdapt1]),cbtgt=zip(x_cbtgt[rangeAdapt1],y_cbtgt[rangeAdapt1]))
+    genReachPlot(fig,axs[0,1],xs[rangeAdapt1],ys[rangeAdapt1],nums[rangeAdapt1],figName,tgt=list(zip(x_target[rangeAdapt1],y_target[rangeAdapt1])),cbtgt=list(zip(x_cbtgt[rangeAdapt1],y_cbtgt[rangeAdapt1])))
     if(len(rangePost1)>0):
         genReachPlot(fig,axs[1,0],xs[rangePost1],ys[rangePost1],nums[rangePost1],"Post1")
 
--- run_shmuelof.sh
+++ run_shmuelof.sh
@@ -13,8 +13,8 @@ seed=0     #makes <more or less> random seed
 
 . ./run_dif_perturb.sh    # . means to source a script, so that it can use variables from the current script
 
-make pert
-./pert --ini=$ini --recalibrateArmCortControl=1 --nsessions=1
+# make pert
+# ./pert --ini=$ini --recalibrateArmCortControl=1 --nsessions=1
 
 if [ $# -ne 0 ]; then
   args1=""
